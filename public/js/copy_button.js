b80021151ae0cb1807f284dd34fec9602b63cd45
yBernier <webyves@hotmail.com>
2019-02-18 18:04:56 +0100

    JS button to copy to clipboard Token

---
+++ public/js/copy_button.js
@@ -0,0 +1,14 @@
+/****************************************
+file: copy_button.js
+JQuery Code for copy to clipboard
+****************************************/
+
+$(document).ready(function() {
+    
+    $('#button-copy-token').on( 'click', function(e){
+        e.preventDefault();
+        $('#inputFbToken').select();
+        document.execCommand('copy');
+    });
+
+});
