2e369823aed5ea6619fbfa3e065538fec2d97810
yBernier <webyves@hotmail.com>
2019-02-22 17:42:19 +0100

    Serialize & Unserialize UserClient
    Serialize & Unserialize UserClient

--- src/Controller/UserClientController.php
+++ src/Controller/UserClientController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\UserClient;
 use App\Repository\UserClientRepository;
 use JMS\Serializer\SerializerInterface;
@@ -19,7 +21,7 @@ class UserClientController extends AbstractController
     public function apiListUserClient(UserClientRepository $repo, SerializerInterface $seri)
     {
         $user = $this->getUser();
-        $clients = $repo->findBy(["userId" => $user->getId()]);
+        $clients = $repo->findBy(["userId" => $user]);
         $data = $seri->serialize($clients, 'json', SerializationContext::create()->setGroups(array('list')));
         return JsonResponse::fromJsonString($data);
     }
@@ -27,35 +29,43 @@ class UserClientController extends AbstractController
     /**
      * @Route("/api/client/{id}", name="user_client_show", requirements={"id"="\d+"}, methods={"GET"})
      */
-    public function apiShowUserClient(UserClient $UserClient, SerializerInterface $seri)
+    public function apiShowUserClient(UserClient $userClient, SerializerInterface $seri)
     {
         $user = $this->getUser();
-        // VERIFIER QUE LE CLIENT APPARTIENT BIEN AU USER
-        $data = $seri->serialize($UserClient, 'json', SerializationContext::create()->setGroups(array('detail')));
+        if ($user !== $userClient->getUserId()) {
+        	return new Response('UNAUTHORIZED ACTION !', Response::HTTP_UNAUTHORIZED);
+        }
+        $data = $seri->serialize($userClient, 'json', SerializationContext::create()->setGroups(array('detail')));
         return JsonResponse::fromJsonString($data);
     }
 
     /**
      * @Route("/api/client/create", name="user_client_create", methods={"POST"})
      */
-    public function apiCreateUserClient(SerializerInterface $seri, Request $request)
+    public function apiCreateUserClient(SerializerInterface $seri, Request $request, EntityManagerInterface $emi)
     {
         $user = $this->getUser();
-        // $clients = $repo->findBy("userId" => $user->getId());
-        // $data = $seri->serialize($clients, 'json', SerializationContext::create()->setGroups(array('list')));
-        $data=null;
-        return JsonResponse::fromJsonString($data);
+        $data = $request->getContent();
+        $userClient = $seri->deserialize($data, 'App\Entity\UserClient', 'json');
+        $userClient->setUserId($user);
+
+        // FAIRE LA VALIDATION DES DONNEES
+        $emi->persist($userClient);
+        $emi->flush();        
+        return new Response('CREATION COMPLETED', Response::HTTP_CREATED);
     }
 
     /**
-     * @Route("/api/client/delete", name="user_client_delete", methods={"POST"})
+     * @Route("/api/client/delete/{id}", name="user_client_delete", methods={"GET"}, requirements={"id"="\d+"})
      */
-    public function apiDeleteUserClient(SerializerInterface $seri, Request $request)
+    public function apiDeleteUserClient(UserClient $userClient, EntityManagerInterface $emi)
     {
         $user = $this->getUser();
-        // $clients = $repo->findBy("userId" => $user->getId());
-        // $data = $seri->serialize($clients, 'json', SerializationContext::create()->setGroups(array('list')));
-        $data=null;
-        return JsonResponse::fromJsonString($data);
+        if ($user !== $userClient->getUserId()) {
+        	return new Response('UNAUTHORIZED ACTION !', Response::HTTP_UNAUTHORIZED);
+        }
+		$emi->remove($userClient);
+	    $emi->flush();
+        return new Response('DELETION COMPLETED', Response::HTTP_ACCEPTED);
     }
 }
